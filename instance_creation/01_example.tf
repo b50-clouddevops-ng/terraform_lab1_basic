eee957e56ecab4f95118b9e859ff265357ac733e
Nimmi gupta <nimmi.gupta@gmail.com>
2022-12-28 22:13:37 -0500

    Install minikube

--- instance_creation/01_example.tf
+++ instance_creation/01_example.tf
@@ -14,3 +14,29 @@ resource "aws_instance" "web" {
 output "aws_instance" {
     value = aws_instance.web.public_ip
 }
+
+#Create security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
